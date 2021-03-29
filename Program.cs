0a67f613519a550e657d2983583bdef23f96ae0b
Kevin Le <kevinle108@gmail.com>
2021-03-29 09:11:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShrinkableWords();
+        }
+
+        // Problem 16
+        static void ShrinkableWords()
+        {
+            // initialize the needed variables
+            List<string> vocab = new List<string>()
+            {
+                "MTAO", "OF", "TEC", "MXD", "T", "O", "AF", "AE", "A",
+                "TAZ", "MTBXAE", "ABCDE", "TAEC", "MTOZE", "MITXFAEC", "MTAFC", "MTSFOC", "MTZABC",
+                "MTAFOC", "MITXFFEC", "XAEC", "MTAFEC", "F", "MTBXAEC", "ZAEC", "MFECFEC", "TAX",
+                "MTXAFEC", "TAFEC", "MXAFECI", "TAE", "ITXAFC", "MTAFOCC", "MITXAFEOC", "D", "MXITABFEC",
+                "AC", "MTAFOC", "MD", "MTXABCD", "OA", "MITXAFEC", "OFMIT", "TED", "MTAEC",
+                "MITXFAEE", "TAF", "TAC", "AEOC", "TA", "MITXAFC", "ITXAFEC", "MITXAFE", "MITAFEC"
+            };
+            string word = "MITXAFEC";
+
+            // create instance of the Class
+            var p1 = new ShrinkableWordsProblem1(word, vocab);
+            var p2 = new ShrinkableWordsProblem2(word, vocab);
+
+            // solve the problem
+            p1.Solve();
+            p2.Solve();
+        }
+    }
+}
---
+++ ShrinkableWordsProblem1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+Problem 16. A shrinkable word is a word that can be reduced down to the empty string by deleting
+one letter at a time such that, at each stage, the remaining string is a word. For example,
+the word “startling” is shirnkable because of this sequence of words:
+startling -> starting -> staring -> string -> sting -> sing -> sin -> in -> i -> (empty)
+Write a function that accepts as input a string and a set of all the words in English, then
+reports whether the input word is shrinkable.
+*/
+
+namespace Day15
+{
+    class ShrinkableWordsProblem1
+    {
+        string _word;
+        List<string> _vocab;
+        //Stack<string> _result;
+
+        public ShrinkableWordsProblem1(string word, List<string> vocab)
+        {
+            _word = word;
+            _vocab = vocab;
+            //_result = new Stack<string>();
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine("Solution by ShrinkableWordsProblem1:");
+            // runs the actual solver function
+            if (ShrinkableWords())
+            {
+                Console.WriteLine("   Yes, your word is shrinkable.");
+            }
+            else Console.WriteLine("   No, your word is not shrinkable.");
+            Console.WriteLine();
+        }
+
+        private bool ShrinkableWords()
+        {
+            if (_word == "") return true;
+            if (!_vocab.Contains(_word)) return false;
+            for (int i = 0; i < _word.Length; i++)
+            {
+                string saved = _word;
+                _word = _word.Remove(i, 1);
+                if (ShrinkableWords()) return true;
+                else
+                {
+                    _word = saved;
+                }
+            }
+            return false;
+        }
+
+        public void PrintVariables()
+        {
+            Console.WriteLine("Printing ShrinkableWords variables:");
+            Console.WriteLine($" _word: {_word}");
+            Console.WriteLine($" _vocab:");
+            foreach (var word in _vocab)
+            {
+                Console.WriteLine($"        {word}");
+            }
+            Console.WriteLine($" _vocab:");
+            foreach (var word in _vocab)
+            {
+                Console.WriteLine($"        {word}");
+            }
+        }
+    }
+}
